ee1aa9cb1935c8ec29e2931f2c45b85e36a59a2f
zzangyu <hellohan95@gmail.com>
2022-05-29 14:55:27 +0900

    static method can use only static variable

--- 1firstjavaproject/src/review_package1/StaticModifierRunner.java
+++ 1firstjavaproject/src/review_package1/StaticModifierRunner.java
@@ -1,21 +1,35 @@
 package review_package1;
+// static method
+
+// static variables의 규칙
+// - 사용해도 될 때
+// - 사용하면 안될 때
 
 class Player {
-	private String name;
+	private static String name; // instance variable, member variable
 	private static int count;
 
 	protected Player(String name) {
 		super();
-		this.name = name;
+		this.setName(name);
 		count++;
 	}
 
-	public int getCount() {
+	static public int getCount() {
+		System.out.println(name);
 		return count;
 	}
 
 	public void setCount(int count) {
 		this.count = count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
@@ -24,10 +38,10 @@ public class StaticModifierRunner {
 
 	public static void main(String[] args) {
 		Player player1 = new Player("Ronaldo");
-		System.out.println(player1.getCount());
+		System.out.println(Player.getCount());
 		Player player2 = new Player("Federer");
-		System.out.println(player1.getCount());
-		System.out.println(player2.getCount());
+		System.out.println(Player.getCount());
+
 	}
 
 }

cf51a4174d1a1c5551e66ea956b51593898ff15d
zzangyu <hellohan95@gmail.com>
2022-05-29 16:24:59 +0900

    Why we have to use getValue not ordinal

--- 1firstjavaproject/src/review/EnumerationRunner.java
+++ 1firstjavaproject/src/review/EnumerationRunner.java
@@ -4,21 +4,32 @@ import java.util.Arrays;
 
 enum Season {
 // Enum - 열거형, 특정 값들에 제한을 두기 위함
-	WINTER, SPRING, SUMMER, FALL;
+	WINTER(1), SPRING(2), SUMMER(3), FALL(4);
+
+	private int value;
+
+	private Season(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
 
 }
 
 public class EnumerationRunner {
 	public static void main(String[] args) {
-		Season season = Season.WINTER;
+		Season season = Season.FALL;
 
 		Season season1 = Season.valueOf("WINTER");
-		System.out.println(season1);
+		System.out.println(season);
 		System.out.println(season1.name());
-		System.out.println(Season.FALL.name());
-		System.out.println(Season.SUMMER.ordinal());
-
+		System.out.println(Season.SPRING.name());
+		System.out.println(Season.SUMMER.ordinal());// 데이터베이스에 저장할 땐 ordinal 사용하면 안된다 !
+		System.out.println(Season.SUMMER.getValue()); // 할당한 값을 가져옴, index 위치가 아니라 정확
 		System.out.println(Arrays.toString(Season.values()));
+
 	}
 
 }

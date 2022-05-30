18c036db641319fb332043e5cf30df357497f15b
zzangyu <hellohan95@gmail.com>
2022-05-30 11:30:56 +0900

    review no.1

--- Day0526/src/com/member/DeleteAction.java
+++ Day0526/src/com/member/DeleteAction.java
@@ -15,28 +15,43 @@ public class DeleteAction implements Action {
 
 		int deleteIndex = -1; // 회원이 없다고 가정
 		MemberVO[] members = MemberMain.members;
+		// 삭제할 회원정보 객체의 인덱스 번호를 찾아내기 위해 MemberMain 클래스의 static 영역에 공유되어 있는 members 배열
+		// 레퍼런스 변수의 레퍼런스 값을 가져옴
 		MemberVO[] temp = null;
-
+		// 원본 배열의 객체에서 제거할 회원 정보 객체를 제거하고 나머지 회원정보 객체를 저장할 배열 객체 레퍼런스 변수명을 temp로 선언함
 		for (int i = 0; i < members.length; i++) {
 			if (members[i].getName().equals(name)) {
 				deleteIndex = i;
+				// 현재 배열 객체에 저장되어 있는 회원 정보 객체중 파라미터 값으로 전송되어 온
+				// name값과 같은 name 속성 값을 가지고 있는 객체의 배열상의 인덱스 번호를 찾아냄
 			}
 		}
 		if (deleteIndex == -1) {
+			// deleteIndex가 -1이라는 것은 파라미터 전송되어 온 name값과 같은 name값을 가지고 있는 회원 정보 객체를 찾지 못했다는
+			// 의미이며
+			// 메시지를 출력하고 메소드를 종료한다.
 			System.out.println("회원 정보가 존재하지 않습니다.");
 			return;
 		}
 		if (members.length >= 1) {
 			temp = new MemberVO[members.length - 1];
+			// temp 배열 객체를 MemberMain클래스에 공유되어 있는 members 배열의 크기보다 하나 작은 크기로 생성
+			// 원본 배열에서 회원정보 객체 하나를 삭제할 것이므로 기존 배열의 크기보다 하나 적게 생성
 			for (int i = 0; i < members.length; i++) {
 				if (i == deleteIndex) {
+					// 현재 인덱스가 삭제하려는 회원정보의 인덱스와 같으면 continue
+					// 회원정보 복사x
 					continue;
 				} else if (i > deleteIndex) {
+					// 현재 인덱스 번호가 삭제하려는 회원 정보의 인덱스 이후이면 삭제하려는 객체의 인덱스 번호가 하나 빠졌기 떄문에 삭제하려는 인덱스보다 큰
+					// 인덱스에
+					// 존재하는 객체들을 앞으로 하나씩 이동시켜서 복사함
 					temp[i - 1] = members[i];
 				} else {
 					temp[i] = members[i];
 				}
 			}
+			// 배열 객체 temp를 MemberMain 클래스에 static으로 공유되어 있는 members변수에 저장함
 			MemberMain.members = temp;
 		}
 	}
---
+++ review/src/com/study/Class_Test001.java
@@ -0,0 +1,22 @@
+package com.study;
+
+/*
+ * 지역 변수는 초기화 과정을 거치지 않으면 사용할 수 없다.
+ * (자동 초기화 기능을 지원하지 않는다.)
+ */
+public class Class_Test001 {
+	static int c; // 전역변수()
+	int d; // 전역변수(인스턴스 멤버변수)
+	// 사용하는 과정에서 초기화를 하지 않을 경우 int형은 컴파일러가 자동으로 0으로 초기화 된다.
+	// 배열이면 null 값, boolean이면 false
+
+	public static void main(String[] args) {
+		int a;
+//		System.out.println(a); compile error 뜸 !
+		int b = 10;
+		System.out.println("b: " + b);
+		System.out.println("c: " + c); // static이라 바로 사용 가능 !
+		System.out.println("d: " + new Class_Test001().d);
+	}
+
+}

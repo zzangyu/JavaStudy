5f243bdf704e1a13c6e29cc08e36a3072238b555
zzangyu <hellohan95@gmail.com>
2022-06-02 13:30:33 +0900

    exception 4th

---
+++ 과제/src/과제/DeckRunner.java
@@ -0,0 +1,25 @@
+package °úÁ¦;
+
+class Card {
+
+	String name;
+	int number;
+
+	protected Card(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
+
+}
+
+class Deck {
+	static Card[] card = new Card[13];
+	int[] deck = new int[52];
+
+}
+
+public class DeckRunner {
+	public static void main(String[] args) {
+
+	}
+}

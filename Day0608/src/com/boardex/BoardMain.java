94a1d13d0cb9dd6191fd298fc56cd9add245d930
zzangyu <hellohan95@gmail.com>
2022-06-08 14:34:13 +0900

    220608 4th

--- Day0608/src/com/boardex/BoardMain.java
+++ Day0608/src/com/boardex/BoardMain.java
@@ -7,7 +7,40 @@ public class BoardMain {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BoardSVC svc = new BoardSVC();
-		svc.writeArticle(sc);
+
+		boolean isStop = false;
+
+		do {
+			System.out.println("메뉴를 선택하세요.");
+			System.out.println("1. 게시판 글 쓰기");
+			System.out.println("2. 게시판 목록 보기");
+			System.out.println("3. 게시판 글 삭제");
+			System.out.println("4. 게시판 글 검색");
+			System.out.println("5. 게시판 글 수정");
+			System.out.println("6. 종료");
+			String menu = sc.next();
+			switch (menu) {
+			case "1":
+				svc.writeArticle(sc);
+				break;
+			case "2":
+				svc.listArticle(sc);
+				break;
+			case "3":
+				svc.deleteArticle(sc);
+				break;
+			case "4":
+				svc.searchArticle(sc);
+				break;
+			case "5":
+				svc.updateArticle(sc);
+				break;
+			case "6":
+				isStop = true;
+			}
+
+		} while (!isStop);
+
 	}
 
 }
--- Day0608/src/com/boardex/BoardSVC.java
+++ Day0608/src/com/boardex/BoardSVC.java
@@ -14,15 +14,15 @@ public class BoardSVC {
 	// 글 쓰기
 	public void writeArticle(Scanner sc) {
 		System.out.println("게시판 글을 작성하세요.");
-		System.out.println("작성자: ");
+		System.out.print("작성자: ");
 		String register = sc.next();
-		System.out.println("이메일: ");
+		System.out.print("이메일: ");
 		String email = sc.next();
-		System.out.println("비밀번호: ");
+		System.out.print("비밀번호: ");
 		String pw = sc.next();
-		System.out.println("제목: ");
+		System.out.print("제목: ");
 		String subject = sc.next();
-		System.out.println("글 내용: ");
+		System.out.print("글 내용: ");
 		String content = sc.next();
 
 		BoardVO boardVO = new BoardVO(register, subject, email, content, pw);
@@ -36,13 +36,84 @@ public class BoardSVC {
 	}
 
 	// 목록 보기
-	public void subjectList() {
-
+	public void listArticle(Scanner sc) {
+		if (boardList.size() > 0) {
+			for (int i = 0; i < boardList.size(); i++) {
+				System.out.println(boardList.get(i));
+			}
+		} else {
+			System.out.println("등록된 게시글이 없습니다.");
+		}
 	}
 
 	// 글 삭제
-	public void deleteSubject() {
+	public void deleteArticle(Scanner sc) {
+		System.out.println("게시글을 제거할 때는 글의 작성자와 비밀번호를 입력하세요.");
+		System.out.print("작성자: ");
+		String register = sc.next();
+		System.out.print("비밀번호: ");
+		String pw = sc.next();
 
+		deleteArticle(register, pw);
+
+	}
+
+	private void deleteArticle(String register, String pw) {
+		// 삭제 기능 구현
+		if (boardList.size() > 0) {
+			int index = -1;
+
+			for (int i = 0; i < boardList.size(); i++) {
+				if (boardList.get(i).getRegister().equals(register)) {
+					if (boardList.get(i).getPw().equals(pw)) {
+						boardList.remove(boardList.get(i));
+						index = 1;
+						System.out.println(register + "님의 게시글이 삭제되었습니다.");
+					}
+				}
+			}
+			if (index == -1) { // 내가 찾는 글이 없을 때, 이름&비밀번호가 맞지 않을 때
+				System.out.println("해당 글의 작성자&비밀번호의 정보와 일치하지 않습니다.");
+				return;
+			}
+
+		} else {
+			System.out.println("등록된 게시글이 없습니다.");
+		}
+
+	}
+
+	public void searchArticle(Scanner sc) {
+		System.out.print("이름을 입력해주세요.");
+		String name = sc.next();
+		if (boardList.size() > 0) {
+			for (int i = 0; i < boardList.size(); i++) {
+				if (boardList.get(i).getRegister().equals(name)) {
+					System.out.println(boardList.get(i));
+				}
+			}
+		} else {
+			System.out.println("등록된 게시글이 없습니다.");
+		}
+	}
+
+	public void updateArticle(Scanner sc) {
+		System.out.println("이름과 비밀번호를 입력해주세요.");
+		System.out.println("이름: ");
+		String register = sc.next();
+		System.out.println("비밀번호: ");
+		String pw = sc.next();
+		if (boardList.size() > 0) {
+			for (int i = 0; i < boardList.size(); i++) {
+				if (boardList.get(i).getRegister().equals(register)) {
+					if (boardList.get(i).getPw().equals(pw)) {
+
+					}
+				}
+			}
+		} else {
+			System.out.println("등록된 게시글이 없습니다.");
+		}
 	}
 
 }

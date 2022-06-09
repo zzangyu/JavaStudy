1d172dd609ad7e00ec3f441c2274ab86cee690b4
zzangyu <hellohan95@gmail.com>
2022-06-09 11:59:21 +0900

    20220609 3rd

--- Day0608/src/com/mapex/mscore/ScoreImpl.java
+++ Day0608/src/com/mapex/mscore/ScoreImpl.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class ScoreImpl implements Score {
 	private Map<String, ScoreVO> map;
@@ -15,16 +17,57 @@ public class ScoreImpl implements Score {
 
 	}
 
+	// 입력된 값이 맞다 1, 값이 없다, 실패 0, 틀리다 -1
 	@Override
 	public int Insert() throws IOException {
+		String hak;
+		System.out.print("학번 입력: ");
+		hak = br.readLine();
+		if (map.containsKey(hak)) {
+			System.out.println("이미 존재하는 학번입니다.");
+			return 0;
+		}
 
-		return 0;
+		ScoreVO vo = new ScoreVO();
+		vo.setHak(hak);
+		System.out.print("이름 입력: ");
+		vo.setName(br.readLine());
+		System.out.print("국어 점수 입력: ");
+		vo.setKor(Integer.parseInt(br.readLine()));
+		System.out.print("영어 점수 입력: ");
+		vo.setEng(Integer.parseInt(br.readLine()));
+		System.out.print("수학 입력: ");
+		vo.setMat(Integer.parseInt(br.readLine()));
+
+		map.put(hak, vo); // 맵에 추가한 것
+
+		return 1;
 	}
 
 	@Override
 	public int Update() throws IOException {
+		String hak;
+		System.out.print("수정할 학번 입력: ");
+		hak = br.readLine();
+		if (!map.containsKey(hak)) {
+			System.out.println("존재하지 않는 학번입니다.");
+			return 0;
+		}
 
-		return 0;
+		ScoreVO vo = new ScoreVO();
+		vo.setHak(hak);
+		System.out.print("이름 입력: ");
+		vo.setName(br.readLine());
+		System.out.print("국어 점수 입력: ");
+		vo.setKor(Integer.parseInt(br.readLine()));
+		System.out.print("영어 점수 입력: ");
+		vo.setEng(Integer.parseInt(br.readLine()));
+		System.out.print("수학 입력: ");
+		vo.setMat(Integer.parseInt(br.readLine()));
+
+		map.put(hak, vo); // 맵에 추가한 것
+
+		return 1;
 	}
 
 	@Override
@@ -41,6 +84,13 @@ public class ScoreImpl implements Score {
 
 	@Override
 	public void WriteAll() {
+		Set<String> set = map.keySet();
+		Iterator<String> it = set.iterator();
+		while (it.hasNext()) {
+			String key = it.next();
+			ScoreVO data = map.get(key);
+			System.out.println(data.toString());
+		}
 
 	}
 
--- Day0608/src/com/mapex/mscore/ScoreVO.java
+++ Day0608/src/com/mapex/mscore/ScoreVO.java
@@ -49,18 +49,9 @@ public class ScoreVO {
 		return tot;
 	}
 
-	private ScoreVO(String name, String hak, int kor, int eng, int mat, int tot) {
-		this.name = name;
-		this.hak = hak;
-		this.kor = kor;
-		this.eng = eng;
-		this.mat = mat;
-		this.tot = tot;
-	}
-
 	@Override
 	public String toString() {
-		String str = String.format("이름:%3s 학번:%7s 국어점수:%3d점 영어점수:%3d점 수학점수:%3d점 총점:%3d점 평균:%5.1f점", name, hak, kor, eng,
+		String str = String.format("학번:%7s 이름:%3s 국어점수:%3d점 영어점수:%3d점 수학점수:%3d점 총점:%3d점 평균:%5.1f점", hak, name, kor, eng,
 				mat, getTot(), (float) getTot() / 3);
 		return str;
 	}

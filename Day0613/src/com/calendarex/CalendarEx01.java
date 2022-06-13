7babd07bf366fa8324aa51fd11ed7d163c0e7cc2
zzangyu <hellohan95@gmail.com>
2022-06-13 15:20:20 +0900

    220613 calendar 1st

--- Day0613/src/com/calendarex/CalendarEx01.java
+++ Day0613/src/com/calendarex/CalendarEx01.java
@@ -18,9 +18,40 @@ public class CalendarEx01 {
 		Calendar gregorianCalendar_1 = new GregorianCalendar();
 		GregorianCalendar gregorianCalendar_2 = new GregorianCalendar();
 
-		while (true) {
-			System.out.println(calendar.DATE + "일");
+		String str1 = String.format("%1$tF %1$tA %1$tT", calendar); // F: 날짜, A: 어느 지역인지, T: 시간
+		System.out.println(str1);
+		int y = calendar.get(Calendar.YEAR);
+		int m = calendar.get(Calendar.MONTH) + 1;
+		int d = calendar.get(Calendar.DATE);
+
+		int w = calendar.get(Calendar.DAY_OF_WEEK); // 그 주의 몇번째 날짜인가 (일요일:0) -> switch case 이용해서 요일 빼내기
+		String week = "";
+		switch (w) {
+		case 1:
+			week = "일요일";
+			break;
+		case 2:
+			week = "월요일";
+			break;
+		case 3:
+			week = "화요일";
+			break;
+		case 4:
+			week = "수요일";
+			break;
+		case 5:
+			week = "목요일";
+			break;
+		case 6:
+			week = "금요일";
+			break;
+		case 7:
+			week = "토요일";
+			break;
+
 		}
+
+		System.out.println(y + "년 " + m + "월 " + d + "일 " + week);
 
 	}
 

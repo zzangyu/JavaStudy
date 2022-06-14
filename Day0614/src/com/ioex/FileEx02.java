aeb3c63afb57d0e94dd0ad560e3b443323193799
zzangyu <hellohan95@gmail.com>
2022-06-14 11:50:34 +0900

    20220614 Stream 2nd

--- Day0614/src/com/ioex/FileEx02.java
+++ Day0614/src/com/ioex/FileEx02.java
@@ -7,6 +7,19 @@ public class FileEx02 {
 	public static void main(String[] args) {
 		File directory = new File("c:/");
 
+		if (directory.exists()) { // 파일 존재 여부 검사
+			if (directory.isDirectory()) { // 디렉토리인지 검사
+				// 현재 디렉토리 내의 모든 파일 리스트 출력, 스트링 배열로 리턴
+				String[] fileNameList = directory.list();
+
+				// 파일 목록 출력
+				for (String fileName : fileNameList) {
+					System.out.println("파일 이름:" + fileName);
+				}
+
+			}
+		}
+
 	}
 
 }

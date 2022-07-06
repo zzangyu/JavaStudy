ef42393fdc6a01d3e882fea5f570400530b01ac7
zzangyu <hellohan95@gmail.com>
2022-07-06 13:42:49 +0900

    Swing Border

--- GraphicProject/src/com/swingex/BorderEx01.java
+++ GraphicProject/src/com/swingex/BorderEx01.java
@@ -1,10 +1,17 @@
 package com.swingex;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.SoftBevelBorder;
 
 public class BorderEx01 extends JFrame {
 
@@ -21,6 +28,19 @@ public class BorderEx01 extends JFrame {
 			jb[i] = new JButton(i + 1 + "번 버튼");
 			con.add(jb[i]);
 		}
+
+		EmptyBorder eb = new EmptyBorder(10, 10, 10, 10); // 테두리 없음
+		jb[0].setBorder(eb);
+		LineBorder lb = new LineBorder(Color.red, 5);
+		jb[1].setBorder(lb);
+		EtchedBorder eeb = new EtchedBorder(); // 경계선이 파여있는 듯한 느낌
+		jb[2].setBorder(eeb);
+		BevelBorder bb = new BevelBorder(BevelBorder.RAISED);
+		jb[3].setBorder(bb);
+		SoftBevelBorder sbb = new SoftBevelBorder(SoftBevelBorder.LOWERED);
+		jb[4].setBorder(sbb);
+		MatteBorder mb = new MatteBorder(5, 10, 15, 20, Color.yellow);
+		jb[5].setBorder(mb);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(300, 200);
